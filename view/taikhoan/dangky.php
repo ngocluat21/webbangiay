31e5984e456c69f959db972bfd4b8204bc64099b
Tungduong0709 <phanduong07092004@gmail.com>
2023-11-22 23:41:56 +0700

    Tài khoản

--- view/taikhoan/dangky.php
+++ view/taikhoan/dangky.php
@@ -1,50 +1,72 @@
-    <div class="content">
-            <!-- Form without bootstrap -->
-        <div class="auth-wrapper">
-            <div class="auth-container">
-                <div class="auth-action-left">
-                    <div class="auth-form-outer">
-                        <h2 class="auth-form-title">
-                            Create Account
-                        </h2>
-                        <div class="auth-external-container">
-                            <div class="auth-external-list">
-                                <ul>
-                                    <li><a href="#"><i class="fa fa-google"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                                </ul>
-                            </div>
-                            <p class="auth-sgt">or use your email for registration:</p>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="shortcut icon" href="assets/images/logo-16x16.png" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/loginandregister.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
+        integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg=="
+        crossorigin="anonymous" />
+</head>
+
+<body>
+    <!-- Form without bootstrap -->
+    <div class="auth-wrapper">
+        <div class="auth-container">
+            <div class="auth-action-left">
+                <div class="auth-form-outer">
+                    <h2 class="auth-form-title">
+                        Create Account
+                    </h2>
+                    <div class="auth-external-container">
+                        <div class="auth-external-list">
+                            <ul>
+                                <li><a href="#"><i class="fa fa-google"></i></a></li>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            </ul>
                         </div>
-                        <form class="login-form" acction="index.php?act=dangky" method="post">
-                            <input type="text" name="username" class="auth-form-input" placeholder="Name">
-                            <input type="email" name="email" class="auth-form-input" placeholder="Email">
-                            <input type="text" name="tel" class="auth-form-input" placeholder="Phone number">
-                            <input type="text" name="address" class="auth-form-input" placeholder="Address">
-                            <div class="input-icon">
-                                <input type="password" name="pass" class="auth-form-input" placeholder="Password">
-                                <i class="fa fa-eye show-password"></i>
-                            </div>
-                            <input type="password" class="auth-form-input" placeholder="Confirm Password">
-                            <label class="btn active">
-                                <input type="checkbox" name='email1' checked>
-                                <i class="fa fa-square-o"></i><i class="fa fa-check-square-o"></i> 
-                                <span> I agree to the <a href="#">Terms</a> and <a href="#">Privacy Policy</a>.</span>
-                            </label>
-                            <div class="footer-action">
-                                <input type="submit" value="Sign Up" class="auth-submit">
-                                <a href="login.html" class="auth-btn-direct">Sign In</a>
-                            </div>
-                        </form>
+                        <p class="auth-sgt">or use your email for registration:</p>
                     </div>
+
+                    <form class="login-form" action="index.php?act=dangky" method="post">
+
+                        <input type="text" name="username" class="auth-form-input" placeholder="Name">
+                        <input type="email" name="email" class="auth-form-input" placeholder="Email">
+                        <input type="text" name="tel" class="auth-form-input" placeholder="Phone number">
+                        <input type="text" name="address" class="auth-form-input" placeholder="Address">
+                        <div class="input-icon">
+                            <input type="password" name="pass" class="auth-form-input" placeholder="Password">
+                            <i class="fa fa-eye show-password"></i>
+                        </div>
+                        <input type="password" class="auth-form-input" placeholder="Confirm Password">
+                        <label class="btn active">
+                            <input type="checkbox" checked>
+                            <i class="fa fa-square-o"></i><i class="fa fa-check-square-o"></i>
+                            <span> I agree to the <a href="#">Terms</a> and <a href="#">Privacy Policy</a>.</span>
+                        </label>
+                        <div class="footer-action">
+                            <input type="submit" name="dangky" value="Sign Up" class="auth-submit">
+                            <a href="login.html" class="auth-btn-direct">Sign In</a>
+                        </div>
+                        <h2 class="thongbao">
+
+                        </h2>
+                    </form>
                 </div>
-                <div class="auth-action-right">
-                    <div class="auth-image">
-                        <img src="assets/images/vector.jpg" alt="login">
-                    </div>
+            </div>
+            <div class="auth-action-right">
+                <div class="auth-image">
+                    <img src="assets/images/vector.jpg" alt="login">
+
                 </div>
             </div>
         </div>
     </div>
+</body>
+
+</html>
